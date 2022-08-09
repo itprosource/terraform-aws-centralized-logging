1475e237c581674f5852cd1df2a81c2c21be910e
Austin Thome <austin.thome1@gmail.com>
2022-08-09 11:01:46 -0500

    Cleaned up old templates

--- old-templates/cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "firehose_log_group" {
-  name = "firehose-log-group"
-  retention_in_days = 731
-
-  tags = {
-    Environment = "production"
-    Application = "serviceA"
-  }
-}
--- old-templates/sqs.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_sqs_queue" "dl_queue" {
-  name                      = "dl-queue"
-  kms_master_key_id = "alias/aws/sqs"
-
-}
-
